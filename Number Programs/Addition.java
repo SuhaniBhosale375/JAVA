56290fe7cf0a2e1ff5bd8a1c7069d8733403a726
SuhaniBhosale375 <suhanibhosale375@gmail.com>
2024-12-04 11:29:02 +0530

    Java Programming

---
+++ Number Programs/Addition.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+
+class Addition
+{
+    public static void main(String arr[])
+    {
+        Scanner sobj = new Scanner(System.in);
+
+        int iNo1 = 0, iNo2 = 0,iAns = 0;
+
+        System.out.println("Enter first number: ");
+        iNo1 = sobj.nextInt();
+
+        System.out.println("Enter second number: ");
+        iNo2 = sobj.nextInt();
+
+        Arithematic aobj = new Arithematic();
+
+        iAns = aobj.Addition(iNo1,iNo2);
+
+        System.out.println("Addition is :"+iAns);
+
+    }
+}
+class Arithematic
+{
+    public int Addition(int i,int j)
+    {
+        int iSum = 0;
+        iSum = i + j;
+        return iSum;
+    }
+}
