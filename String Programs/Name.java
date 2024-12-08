150d9f738b884af5ce80c10d43b88ff63aeba5a2
SuhaniBhosale375 <suhanibhosale375@gmail.com>
2024-12-08 12:08:53 +0530

    Java Programming

---
+++ String Programs/Name.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class Name
+{
+    public static void main(String arg[])
+    {
+        Scanner sobj = new Scanner(System.in);
+        System.out.println("Please enter your full name :");
+        String str = sobj.nextLine();
+
+        System.out.println("Welcom "+str);
+    }
+}
