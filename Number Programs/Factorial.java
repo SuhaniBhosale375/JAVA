9f743b1b340765433457dd066bee3fe676095535
SuhaniBhosale375 <suhanibhosale375@gmail.com>
2024-12-24 22:23:21 +0530

    Java Programs

---
+++ Number Programs/Factorial.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class Factorial
+{
+    public static void main(String arr[])
+    {
+        Scanner sobj = new Scanner(System.in);
+
+        System.out.println("Enetr a number :");
+        int iValue = sobj.nextInt();
+
+        Numbers nobj = new Numbers();
+
+        nobj.CountFactorial(iValue);
+    }
+}
+class Numbers
+{
+    
+}
