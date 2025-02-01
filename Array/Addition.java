9a3bd0e638f34ca7a1433bcf20794a64c2e606a6
SuhaniBhosale375 <suhanibhosale375@gmail.com>
2025-02-01 13:59:50 +0530

    Java Program

---
+++ Array/Addition.java
@@ -0,0 +1,66 @@
+import java.util.*;
+
+class Addition
+{
+    public static void main(String arr[])
+    {
+        Scanner sobj = new Scanner(System.in);
+
+        System.out.println("Enter the size of array :");
+        int iSize = sobj.nextInt();
+
+        Maths mobj = new Maths(iSize);
+
+        mobj.Accept();
+        mobj.Display();
+        int iRet = mobj.Add_N_Numbers();
+
+        System.out.println("Addition is : "+iRet);
+
+    }
+}
+
+class Maths
+{
+    public int Arr[];
+
+    public Maths(int iSize)
+    {
+        Arr = new int[iSize];
+    }
+
+    public void Accept()
+    {
+        Scanner sobj = new Scanner(System.in);
+
+        System.out.println("Enter the elements of the array that we want to perform the operation:");
+
+        for(int i = 0; i < Arr.length; i++)
+        {
+            System.out.println("Enter elements no :"+(i+1));
+            Arr[i] = sobj.nextInt();
+        }       
+
+    }
+
+    public void Display()
+    {
+        System.out.println("Array Elements are :");
+
+        for(int i = 0; i < Arr.length; i++)
+        {
+            System.out.println(Arr[i]+"\t");
+        }
+    }
+    public int Add_N_Numbers()
+    {
+        int Ans = 0;
+
+        for(int i = 0; i < Arr.length; i++)
+        {
+            Ans = Ans + Arr[i];
+        }
+
+        return Ans;
+    }
+}
