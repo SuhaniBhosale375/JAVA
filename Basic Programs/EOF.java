cc064cbed723702c4df755626a301de817cc182f
SuhaniBhosale375 <suhanibhosale375@gmail.com>
2025-02-21 22:13:05 +0530

    Jav Programs

---
+++ Basic Programs/EOF.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args)
+    {
+        Scanner scan = new Scanner(System.in);
+        
+        String s1 = scan.nextLine();
+        String s2 = scan.nextLine();
+        String s3 = scan.nextLine();
+        
+        System.out.println("1 "+s1);
+        System.out.println("2 "+s2);
+        System.out.println("3 "+s3);
+        
+        
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        
+        scan.close();
+        
+    }
+}
