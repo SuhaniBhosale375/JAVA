cb2bc5fb7d0a3383f3aa27a9edc88dc26789a9d3
SuhaniBhosale375 <suhanibhosale375@gmail.com>
2025-05-21 23:12:59 +0530

    Java Programming

---
+++ Java programs/Package.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+class Package
+{
+    public static void main(String arg[])
+    {
+        Scanner sobj = new Scanner(System.in); 
+
+        System.out.println("Enter first number : ");
+        int iNo = 
+    }
+}
