5c0a814dc2d3f75dbce6bb549385d568859deb85
SuhaniBhosale375 <suhanibhosale375@gmail.com>
2025-05-21 23:14:54 +0530

    Java Programming

---
+++ Java programs/StaticDemo.java
@@ -0,0 +1,65 @@
+class StaticDemo
+{
+    static
+    {
+        System.out.println("Inside the static block of staticDemo class which contains main");
+    }
+    public StaticDemo()
+    {
+        System.out.println("Inside the constructor of StaticDemo");
+    }
+    public static void main(String arg[])
+    {
+       
+        System.out.println("Inside main");
+        System.out.println("Value of static No3 : "+Demo.No3);      // Demo::No3
+        System.out.println("Value of static No4 : "+Demo.No4);      // Demo::No4
+        Demo.gun();
+ 
+        Demo obj1 = new Demo();
+        Demo obj2 = new Demo();
+
+        obj1.fun();
+    }
+}
+
+class Demo
+{
+    public int No1;              //Non-static Characteristics
+    public int No2;              //Non-static Characteristics
+    public static int No3;       //static Characteristics
+    public static int No4;       //static Characteristics
+
+    static 
+    {
+        System.out.println("Inside static block of Demo class");
+        No3 = 51;
+        No4 = 101;
+    }
+    public Demo()
+    {
+        System.out.println("Inside the Constructor");
+        No1 = 11;
+        No2 = 21;
+    }
+    public void fun()
+    {
+        System.out.println("Inside non-static method fun");
+
+        System.out.println("Value of No1 : "+this.No1);
+        System.out.println("Value of No2 : "+this.No2);
+        System.out.println("Value of No3 : "+this.No3);
+        System.out.println("Value of No4 : "+this.No4);
+
+    }
+    public static void gun()
+    {
+        System.out.println("Inside static method gun");
+        
+        //System.out.println("Value of No1 : "+No1);
+        //System.out.println("Value of No2 : "+No2);
+        System.out.println("Value of No3 : "+No3);
+        System.out.println("Value of No4 : "+No4);
+
+    }
+}
