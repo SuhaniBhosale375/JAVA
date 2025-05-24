c873ff34d4fed100ee6180e37570540dfb95b6a5
SuhaniBhosale375 <suhanibhosale375@gmail.com>
2025-05-24 22:45:40 +0530

    Java Programming

---
+++ Java programs/Database.java
@@ -0,0 +1,28 @@
+import java.sql.*;
+
+class Database
+{
+    public static void main(String A[]) throws Exception
+    {
+        String URL = "jdbc:mysql://localhost:3306/Fork";
+        String Username = "root";
+        String Password = "root";
+        String Query = "select * from student";
+
+        Connection cobj = DriverManager.getConnection(URL,Username,Password);
+
+        Statement sobj = cobj.createStatement();
+
+        ResultSet robj = sobj.executeQuery(Query);
+
+        while(robj.next()) 
+        {
+            System.out.println("RID : "+robj.getInt("RID"));
+            System.out.println("Name : "+robj.getString("Name"));
+            System.out.println("City : "+robj.getString("City"));
+            System.out.println("Marks : "+robj.getInt("Marks"));
+
+        }
+
+    }
+}
