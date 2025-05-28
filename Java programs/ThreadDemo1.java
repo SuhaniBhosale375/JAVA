b38ba59fae55ebca34c17a1b4e02cb38c058eb8d
SuhaniBhosale375 <suhanibhosale375@gmail.com>
2025-05-28 17:55:56 +0530

    Java Programming

---
+++ Java programs/ThreadDemo1.java
@@ -0,0 +1,10 @@
+class ThreadDemo1
+{
+    public static void main(String arr[])
+    {
+        System.out.println("Inside main method");
+
+        System.out.println("Name ofcurrent thread is: "+Thread.currentThread().getName());
+
+    }
+}
