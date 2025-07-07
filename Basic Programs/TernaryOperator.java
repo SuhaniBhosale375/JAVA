6fe1261545ab4f200f6842626cec73de9f7a3e1d
SuhaniBhosale375 <suhanibhosale375@gmail.com>
2025-07-07 20:55:27 +0530

    Java Programming

---
+++ Basic Programs/TernaryOperator.java
@@ -0,0 +1,19 @@
+public class TernaryOperator {
+
+    public static void main(String arr[])
+    {
+        int n = 4;
+
+        int result = 0;
+
+        // if(n%2==0)
+        //     result = 10;
+
+        // else
+        //     result = 20;
+        result = n%2==0 ? 10 : 20;
+
+            System.out.println(result);
+    }
+    
+}
